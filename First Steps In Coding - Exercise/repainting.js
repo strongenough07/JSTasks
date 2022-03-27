3640e5b9807a078fab47beab8cba1867ff2d1df2
strongenough07 <79000625+strongenough07@users.noreply.github.com>
2022-03-27 18:01:55 +0300

    Repainting

---
+++ First Steps In Coding - Exercise/repainting.js
@@ -0,0 +1,28 @@
+function repainting (input) {
+ const nylonPerSqMeter = 1.5
+ const paintPerLiter = 14.5
+ const thinerPerLiter = 5
+
+ const extraPaintPercent = 0.1
+ const extraNaylonPerSqlMeter = 2
+ const bagPrice = 0.4
+
+ const nylon = Number(input[0])
+ const paint = Number(input[1])
+ const thiner = Number(input[2])
+ const workHours = Number(input[3])
+
+ const nylonCost = (nylon + extraNaylonPerSqlMeter) * nylonPerSqMeter
+ const paintCost = (paint + (paint * extraPaintPercent)) * paintPerLiter
+ const thinerCost = thiner * thinerPerLiter
+ const materialCost = nylonCost + paintCost + thinerCost + bagPrice
+ const workerCostPerHour = materialCost * 0.3
+ const totalWorkCost = workHours * workerCostPerHour
+ const totalCost = totalWorkCost + materialCost
+ console.log(totalCost);
+}
+
+repainting(["10 ",
+"11 ",
+"4 ",
+"8 "])
