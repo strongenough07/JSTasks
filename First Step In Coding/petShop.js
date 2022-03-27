78276cb5d6ce0e565d8bc841856e1682d20573c7
strongenough07 <79000625+strongenough07@users.noreply.github.com>
2022-03-27 11:09:19 +0300

    Pet Shop

---
+++ First Step In Coding/petShop.js
@@ -0,0 +1,10 @@
+function petShop (input) {
+    let foodDog = Number(input[0])
+    let foodCat = Number(input[1])
+    let sum = foodDog * 2.5 + foodCat * 4
+
+    console.log(`${sum} lv.`);
+}
+
+petShop(["5 ",
+"4 "])
