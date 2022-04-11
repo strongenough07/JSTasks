4120f2ae8b16337714a22f67404da35d31cc2b96
strongenough07 <79000625+strongenough07@users.noreply.github.com>
2022-04-11 14:10:55 +0300

    Clever Lily

---
+++ For Loop - Exercice/cleverLily.js
@@ -0,0 +1,30 @@
+function cleverLily (input) {
+   let age = Number(input[0])
+   let washingMachinePrice = Number(input[1])
+   let toyPrice = Number(input[2])
+   let tempMoney = 10
+   let money = 0
+   let toyCount = 0
+
+   for (let i = 1; i <= age; i++) {
+       if (i % 2 === 0) {
+            money += tempMoney
+            tempMoney += 10
+            money -= 1
+       } else {
+           toyCount += 1
+       }
+   }
+   money += toyCount * toyPrice
+   let difference = Math.abs(money - washingMachinePrice)
+   if (money >= washingMachinePrice) {
+       console.log(`Yes! ${difference.toFixed(2)}`);
+   } else {
+       console.log(`No! ${difference.toFixed(2)}`);
+   }
+}
+
+
+cleverLily(["10",
+"170.00",
+"6"])
