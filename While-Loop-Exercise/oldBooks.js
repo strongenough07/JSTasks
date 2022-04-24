62a051b16a55cad526c255790fab6f5b26fa83ac
strongenough07 <79000625+strongenough07@users.noreply.github.com>
2022-04-24 11:49:54 +0300

    Old Books

---
+++ While-Loop-Exercise/oldBooks.js
@@ -0,0 +1,29 @@
+function oldBooks (input) {
+    let searchedBook = input[0];
+
+    let index = 1
+    let bookIsFound = false;
+
+    let nextBookName = input[index]
+    while (nextBookName !== "No More Books") {
+        if (nextBookName === searchedBook) {
+            bookIsFound = true;
+            break;
+        }
+        index++;
+        nextBookName = input[index]
+    }
+    if (bookIsFound) {
+        console.log(`You checked ${index - 1} books and found it.`);
+    } else {
+        console.log("The book you search is not here!");
+        console.log(`You checked ${index - 1} books.`);
+    }
+}
+
+oldBooks(["The Spot",
+"Hunger Games",
+"Harry Potter",
+"Torronto",
+"Spotify",
+"No More Books"])
