8303907cdf1e187b02019c43e6993e6e1f366205
strongenough07 <79000625+strongenough07@users.noreply.github.com>
2022-05-08 23:09:14 +0300

    Sum Prime Non Prime

---
+++ Nested-Loops-Exercise/sumPrimeNonPrime.js
@@ -0,0 +1,37 @@
+function sumPrimeNonPrime (input) {
+    let index = 0;
+    let primeSum = 0;
+    let nonPrimeSum = 0;
+
+    while (input[index] !== "stop") {
+        let currentNum = Number(input[index]);
+        if (currentNum < 0 ) {
+            console.log("Number is negative.");
+        } else {
+            let isPrime = true;
+            for (let i = 2; i < currentNum; i++) {
+                if (currentNum % i === 0) {
+                    isPrime = false;
+                    break;
+                }
+                
+            }
+            if (isPrime) {
+                primeSum += currentNum
+            } else {
+                nonPrimeSum += currentNum
+            }
+        }
+        index++;        
+    }
+    console.log(`Sum of all prime numbers is: ${primeSum}`);
+    console.log(`Sum of all non prime numbers is: ${nonPrimeSum}`);
+}
+
+sumPrimeNonPrime(["3",
+"9",
+"0",
+"7",
+"19",
+"4",
+"stop"])
