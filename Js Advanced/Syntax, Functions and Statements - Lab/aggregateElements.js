99eb22db169d12c62c12ea16db7acd00dab3711a
strongenough07 <systembk8445@gmail.com>
2022-05-22 13:15:23 +0300

    Aggregate Elements

---
+++ Js Advanced/Syntax, Functions and Statements - Lab/aggregateElements.js
@@ -0,0 +1,15 @@
+function aggregateElements(items) {
+  let sum = 0;
+  let sum2 = 0;
+  let concatResult = "";
+  for (let i = 0; i < items.length; i++) {
+    const currentItem = items[i];
+    sum += currentItem;
+    concatResult += currentItem;
+    sum2 += 1 / currentItem;
+  }
+  console.log(sum);
+  console.log(sum2);
+}
+
+aggregateElements([1, 2, 3]);
