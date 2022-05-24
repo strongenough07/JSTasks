4d0477eff7e454d051800ea11703ae9d42243201
strongenough07 <systembk8445@gmail.com>
2022-05-24 13:27:55 +0300

    Same Numbers

---
+++ Js Advanced/Syntax, Functions and Statements - Exercise/sameNumbers.js
@@ -0,0 +1,18 @@
+function sameNumbers(num) {
+  const numAsText = num.toString();
+  let same = true;
+  let sum = 0;
+
+  for (let i = 0; i < numAsText.length; i++) {
+    if (i < numAsText.length - 1) {
+      if (numAsText[i] != numAsText[i + 1]) {
+        same = false;
+      }
+    }
+    sum += Number(numAsText[i]);
+  }
+  console.log(same);
+  console.log(sum);
+}
+
+sameNumbers(2222222);
