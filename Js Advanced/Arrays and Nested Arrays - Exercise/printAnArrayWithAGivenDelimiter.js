6ab99f9779e05429c309a10f1a5a0a40a38f3545
strongenough07 <systembk8445@gmail.com>
2022-05-29 16:21:37 +0300

    Print an Array with a Given Delimiter

---
+++ Js Advanced/Arrays and Nested Arrays - Exercise/printAnArrayWithAGivenDelimiter.js
@@ -0,0 +1,8 @@
+function printAnArrayWithAGivenDelimiter(array, delimiter) {
+  console.log(array.join(delimiter));
+}
+
+printAnArrayWithAGivenDelimiter(
+  ["How about no?", "I", "will", "not", "do", "it!"],
+  "_"
+);
